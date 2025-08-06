c4bbf8cf5a5cb00327ce337e400fcfecb53ef32f
77menzs <77menzs@gmail.com>
2025-08-06 14:20:01 +0200

    Add project controller

---
+++ SkillSnap.Api/Controllers/ProjectController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillSnap.Shared.Models;
+
+namespace SkillSnap.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private readonly SkillSnapContext _context;
+
+        public ProjectController(SkillSnapContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            var projects = _context.Projects.ToList();
+            return projects != null && projects.Any() ? Ok(projects) : NotFound("No projects found.");
+        }
+
+        [HttpPost]
+        public IActionResult CreateProject([FromBody] Project project)
+        {
+            if (project == null)
+            {
+                return BadRequest("Project cannot be null.");
+            }
+
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetProjects), new { id = project.Id }, project);
+        }
+    }
+}
