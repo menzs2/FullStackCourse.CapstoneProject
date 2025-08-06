723f496409a8a40e77b7b40a3d4a41950c6e7dce
77menzs <77menzs@gmail.com>
2025-08-06 12:53:51 +0200

    Add Dbcontext

--- SkillSnap.Api/Program.cs
+++ SkillSnap.Api/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<SkillSnapContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("SkillSnapDatabase")));
+    options.UseSqlite("Data Source=skillsnap.db"));
 
 var app = builder.Build();
 
